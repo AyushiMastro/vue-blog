75b9d98807de0ac6869cdc6c2ccabfb0db58def9
ayushimastro <25ayushi1995@gmail.com>
2020-10-06 10:51:15 +0530

    initialcommit

---
+++ components/blog.vue
@@ -0,0 +1,161 @@
+<template>
+  <div class="position-relative">
+    <div class="Blog-content">
+      <div class>
+        <h3>Blog</h3>
+      </div>
+      <div class="d-flex justify-content-between">
+        <p>Create a Blog Post</p>
+        <ul class="dashboard">
+          <li>Home</li>
+          <li>Dashboard</li>
+          <li>Add a Blog</li>
+        </ul>
+      </div>
+
+      <div class="row">
+        <div class="col-sm-9">
+          <section class>
+            <quill-editor
+              ref="editor"
+              @blur="onEditorBlur($event)"
+              @focus="onEditorFocus($event)"
+              @ready="onEditorReady($event)"
+            />
+
+            <div class="bg-grey">
+              <input id="file" type="file" />
+            </div>
+            <client-only>
+              <div ref="editor" class="ql-editor" contenteditable></div>
+            </client-only>
+          </section>
+        </div>
+        <div class="col-sm-3">
+          <div class="button-box">
+            <b-button variant="primary">Save and Publish</b-button>
+          </div>
+          <div class="add-field">
+            <div>
+              <label class="text-label">Tags</label>
+              <b-form-input v-model="text" placeholder="Add Keywords"></b-form-input>
+              <div class="add-outer">+</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "quill-example-nuxt",
+  data() {
+    return {
+      editorOption: {
+        // Some Quill options...
+        theme: "snow",
+        modules: {
+          toolbar: [
+            ["bold", "italic", "underline", "strike"],
+            [{ list: "ordered" }, { list: "bullet" }],
+            [{ header: [1, 2, 3, 4, 5, 6, false] }],
+            [{ color: [] }, { background: [] }],
+            [{ font: [] }],
+            [{ align: [] }],
+            ["link", "image"],
+            ["clean"]
+          ]
+        }
+      }
+    };
+  },
+  mounted() {
+    // console.log(
+    //   "App inited, the Quill instance object is:",
+    //   this.$refs.editor.quill
+    // );
+    console.log(this.$refs["editor"].quill.addContainer("ql-xtz"));
+  },
+  methods: {
+    onEditorBlur(editor) {
+      console.log("editor blur!", editor);
+    },
+    onEditorFocus(editor) {
+      console.log("editor focus!", editor);
+    },
+    onEditorReady(editor) {
+      console.log("editor ready!", editor);
+    }
+  }
+};
+</script>
+
+<style>
+.width100 {
+  width: 100%;
+}
+.dashboard li {
+  list-style: none;
+  display: inline-block;
+  font-size: 12px;
+}
+.Blog-content {
+  padding: 20px;
+}
+.quill-editor {
+  min-height: 200px;
+  max-height: 400px;
+  overflow-y: auto;
+}
+.button-box {
+  border: 1px solid #007bff;
+  border-radius: 3px;
+  display: flex;
+  justify-content: center;
+  padding: 16px;
+}
+.btn.btn-primary {
+  font-size: 12px;
+}
+.add-field {
+  position: relative;
+}
+.text-label {
+  font-size: 14px;
+  padding-top: 8px;
+}
+.form-control {
+  font-size: 11px;
+  height: 36px;
+}
+.add-outer {
+  width: 26px;
+  height: 26px;
+  background-color: #007bff;
+  border-radius: 2px;
+  color: #fff;
+  justify-content: center;
+  display: flex;
+  align-items: center;
+  position: absolute;
+  right: 4px;
+  top: 42px;
+}
+.quill-editor {
+  min-height: 0;
+}
+.fas.fa-plus-square {
+  font-size: 30px;
+}
+.bg-grey {
+  background: url("https://static-exp1.licdn.com/sc/h/9umi21lvbdkhlz1fzfto0ks1p")
+    center center no-repeat #e4e4e4;
+  height: 200px;
+}
+#file {
+  opacity: 0;
+  height: 200px;
+  width: 100%;
+}
+</style>
---
+++ layouts/navbar.vue
@@ -0,0 +1,109 @@
+<template>
+  <div class="">
+    <b-navbar toggleable="lg" type="dark" variant>
+      <b-navbar-brand href="#">Giggs</b-navbar-brand>
+
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav>
+          <b-nav-item href="#">Link</b-nav-item>
+          <b-button variant="outline-success">Buyer</b-button>
+        </b-navbar-nav>
+
+        <!-- Right aligned nav items -->
+        <b-navbar-nav class="ml-auto">
+          <ul class="navbar-links">
+            <li>
+              <i class="fas fa-border-all"></i>
+            </li>
+
+            <li>
+              <i class="fas fa-question-circle"></i>
+            </li>
+            <li>
+              <i class="far fa-bell"></i>
+            </li>
+            <li>
+              <i class="far fa-envelope"></i>
+            </li>
+          </ul>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+    <div class="d-flex">
+      <div class="sidebar">
+        <ul>
+          <li>
+            <i class="fas fa-envelope-square"></i> Messages
+          </li>
+          <li>
+            <i class="far fa-star"></i> Collections
+          </li>
+          <li>
+            <i class="fas fa-calendar-week"></i> Calender
+          </li>
+          <span class="side-text">Manage</span>
+          <li>
+            <i class="fab fa-first-order-alt"></i> Order
+          </li>
+          <li>
+            <i class="fas fa-notes-medical"></i> Giggs
+          </li>
+          <span class="side-text">Tools</span>
+          <li>
+            <i class="fas fa-suitcase-rolling"></i> Blog
+          </li>
+          <li>Blogs</li>
+          <li>Add a Blog</li>
+          <span class="side-text">Tools</span>
+          <li>
+            <i class="fas fa-cog"></i> Settings
+          </li>
+          <li>
+            <i class="fas fa-power-off"></i> Logout
+          </li>.
+        </ul>
+      </div>
+      <Nuxt />
+    </div>
+  </div>
+</template>
+<script>
+export default {};
+</script>
+
+<style>
+
+.navbar {
+  background-color: #fff;
+  border-top: 4px solid #000;
+  box-shadow: 0px 16px #f2f2f2;
+}
+.navbar-brand {
+  color: #000 !important;
+}
+.navbar-links li {
+  list-style-type: none;
+  display: inline-block;
+  padding-right: 14px;
+}
+.side-text {
+  color: blue;
+}
+.sidebar {
+  width: 250px;
+  background: #fff;
+  box-shadow: 0px 16px #f2f2f2;
+  height: 80vh;
+  position: relative;
+}
+.sidebar ul {
+  padding-top: 1rem;
+}
+.sidebar ul li {
+  list-style: none;
+  padding-bottom: 20px;
+  font-size: 13px;
+}
+</style>
